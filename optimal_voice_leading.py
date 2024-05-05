21f5cdd5d71c25a23751abfe65ae6d9103898266
Will Dickerson <will.e.dickerson@gmail.com>
2024-05-05 14:02:41 -0500

    add range arg

--- optimal_voice_leading.py
+++ optimal_voice_leading.py
@@ -18,6 +18,7 @@ def main():
         --output-pdf: Output the optimal voice leading sequence to a PDF file with standard musical notation.
         --name: Specify the name of the song for output files.
         --chords: Specify the chord chart as a comma-separated string.
+        --range: Specify the note range as a comma-separated string (e.g., "40,90").
     """
     parser = argparse.ArgumentParser(description="Optimal Voice Leading")
     parser.add_argument("--play", action="store_true", help="Initialize and play the MIDI sequence")
@@ -26,6 +27,7 @@ def main():
     parser.add_argument("--output-pdf", action="store_true", help="Output the optimal voice leading sequence to a PDF file with standard musical notation")
     parser.add_argument("--name", type=str, help="Specify the name of the song for output files")
     parser.add_argument("--chords", type=str, default="", help="Specify the chord chart as a comma-separated string")
+    parser.add_argument("--range", type=str, default="40,90", help="Specify the note range as a comma-separated string (e.g., '40,90')")
     args = parser.parse_args()
 
     if args.chords:
@@ -35,7 +37,12 @@ def main():
         chords = ["B", "D", "G", "Bb", "Eb", "Eb", "Am", "D", "G", "Bb", "Eb", "F#", "B", "B", "Fm", "Bb", "Eb", "Eb", "Am", "D", "G", "G", "C#m", "F#", "B", "B", "Fm", "Bb", "Eb", "Eb", "C#m", "F#"]
         print("No chord chart provided. Running on the Giant Steps progression.")
 
-    midi_range = (40, 90)
+    if args.range:
+        midi_range = tuple(map(int, args.range.split(",")))
+    else:
+        # Default guitar range
+        midi_range = (40, 90)
+
     voice_leading_graph = build_voice_leading_graph(chords, midi_range)
     start_chords = [(node[0], node[1], node[2], node[3]) for node in voice_leading_graph.nodes() if node[0] == 0 and node[1] == chords[0]]
     optimal_path_midis = find_optimal_voice_leading(voice_leading_graph, start_chords, chords)
